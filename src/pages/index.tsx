3236af140b8c52b0b52b160c6e34fc4e5bcae752
Armando Perez <armandlord98@gmail.com>
2023-04-18 22:34:02 -0600

    agrega funcionalidad para elegir alimentos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,115 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+interface PropsMeals {
+  userOneList: string[];
+  userTwoList: string[];
+}
 
 export default function Home() {
+  const [meals, setMeals] = useState<PropsMeals>({
+    userOneList: [],
+    userTwoList: [],
+  });
+
+  const [coincidences, setCoincidences] = useState<string[]>([]);
+
+  const handleSelectUserOne = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    setMeals((prevState) => ({
+      ...prevState,
+      userOneList: [...prevState.userOneList, value],
+    }));
+  };
+
+  const handleSelectUserTwo = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    setMeals((prevState) => ({
+      ...prevState,
+      userTwoList: [...prevState.userTwoList, value],
+    }));
+  };
+
+  const combinationOfMeals = () => {
+    const { userOneList, userTwoList } = meals;
+    // devulva las coincidencias
+    const coincidences = userOneList.filter((meal) =>
+      userTwoList.includes(meal)
+    );
+
+    setCoincidences(coincidences);
+  };
+
+  console.log(meals);
+  const mealsList = [
+    "Pasta",
+    "Pizza",
+    "Hamburguesa",
+    "Sushi",
+    "Tacos",
+    "Tortas",
+  ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section className="flex flex-col items-center justify-around w-full h-1/2">
+        <h1 className="text-stone-100 text-3xl">MealMeate</h1>
+        {/* <span className="text-stone-100 text-xl text-center">
+          MealMate es una aplicación que te ayudará a crear combinaciones
+          aleatorias de comidas de manera sencilla. Solo tienes que crear dos
+          listas con tus comidas favoritas: una lista para ti y otra para tu
+          pareja. La aplicación se encargará de generar combinaciones al azar a
+          partir de estas listas para ayudarte a planificar tus comidas de
+          manera divertida y novedosa. ¡Nunca más tendrás que preguntarte qué
+          cocinar para la cena!
+        </span> */}
+
+        {/* construye inputs que mapee mealsList */}
+        <div className="flex items-center justify-around w-full h-1/2">
+          <div className="flex flex-col items-center justify-around w-full h-1/2">
+            <h2 className="text-stone-100 text-xl">Usuario 1</h2>
+            <select
+              className="bg-stone-500 text-stone-100 text-xl p-2 rounded-md"
+              onChange={handleSelectUserOne}
+            >
+              <option value="">Selecciona una comida</option>
+              {mealsList.map((meal) => (
+                <option key={meal} value={meal}>
+                  {meal}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col items-center justify-around w-full h-1/2">
+            <h2 className="text-stone-100 text-xl">Usuario 2</h2>
+            <select
+              className="bg-stone-500 text-stone-100 text-xl p-2 rounded-md"
+              onChange={handleSelectUserTwo}
+            >
+              <option value="">Selecciona una comida</option>
+              {mealsList.map((meal) => (
+                <option key={meal} value={meal}>
+                  {meal}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={combinationOfMeals}
+          className="bg-stone-500 text-stone-100 text-xl p-2 rounded-md"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          Generar combinación
+        </button>
+      </section>
+      <section className="flex flex-col items-center justify-around w-full h-1/2">
+        <h2 className="text-stone-100 text-xl">Combinaciones:</h2>
+        <div className="flex flex-col items-center justify-around w-full h-1/2">
+          {coincidences.map((coincidence) => (
+            <span key={coincidence} className="text-stone-100 text-xl">
+              {coincidence}
             </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
